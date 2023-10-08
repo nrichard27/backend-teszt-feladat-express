190728aae84183a3befdd2da5530ff83a9c04d0d
nrichard27 <thenagyrichard@gmail.com>
2023-10-09 01:13:27 +0200

    Create auth exceptions

---
+++ src/exceptions/auth.exception.ts
@@ -0,0 +1,39 @@
+export class AuthException extends Error {
+    constructor(
+        readonly status: number,
+        readonly message: string,
+        readonly code: number,
+    ) {
+        super();
+    }
+}
+
+export class UnauthorizedException extends AuthException {
+    constructor() {
+        super(401, 'Unauthorized', 800);
+    }
+}
+
+export class WrongCredentialsException extends AuthException {
+    constructor() {
+        super(401, 'Wrong credentials', 801);
+    }
+}
+
+export class UsernameInUseException extends AuthException {
+    constructor() {
+        super(401, 'Username is already in use', 807);
+    }
+}
+
+export class EmailInUseException extends AuthException {
+    constructor() {
+        super(401, 'Email is already in use', 808);
+    }
+}
+
+export class ForbiddenException extends AuthException {
+    constructor() {
+        super(403, 'Forbidden', 813);
+    }
+}
